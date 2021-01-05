a987ebc258ac1c4b40027c48356890d7af481a07
BULENT KAYICI <bulentkyc@MacBook-Pro.fritz.box>
2021-01-05 13:37:20 +0100

    some update

--- main.js
+++ main.js
@@ -1,30 +1,32 @@
 //Part 1 : Logic
-let players = {
-    player1: {
+let players = [
+    {
         userName: 'Antonis',
         color: 'red',
-        positions: [0,0,0,0],
+        positions: [1,4,0,0],
         allAtHome: true
     },
-    player2: {
+    {
         userName: 'James',
         color: 'green',
-        positions: [0,0,0,0],
+        positions: [10,0,9,0],
         allAtHome: true
     },
-    player3: {
+    {
         userName: 'Nikolaos',
         color: 'yellow',
         positions: [0,0,0,0],
         allAtHome: true
     },
-    player4: {
+    {
         userName: 'Pio',
         color: 'blue',
         positions: [0,0,0,0],
         allAtHome: true
     }
-}
+];
+
+let currentPlayer = 0;
 
 function rollDice() {
     const dice =  Math.floor(Math.random() * 6) + 1;
@@ -32,17 +34,37 @@ function rollDice() {
     return dice;
 }
 
-function diceChecker(dice, currentPlayer) {
+function diceChecker(dice) {
     if (dice < 6 && players[currentPlayer]['allAtHome']) {
         alert('Hey '+ players[currentPlayer]['userName'] +' Please wait for 6! You have ' + dice);
-    } else if (dice == 6 && players[currentPlayer]['allAtHome']) {
+    } else if (dice == 6 && players[currentPlayer]['allAtHome']) { //First 6
+        players[currentPlayer]['positions'] = [1,0,0,0];
         alert('Congrats you can go out!');
     }
 }
 
 //Part 2: DOM Manipulation
+function render () {
+    for (let index = 0; index < players.length; index++) {
+        const element = players[index];
+        
+        for (let index = 0; index < element.positions.length; index++) {
+            const item = element.positions[index];
+            if (item > 0) {
+                    document.getElementById('pos'+item).innerHTML = '<i class="fas fa-chess-pawn"></i>';
+                    document.getElementById('pos'+item).classList.add(element.color);
+                    document.getElementById('pos'+item).classList.remove('white');
+                
+            }
+        }
+    }
+}
+
+render();
+
 function showDice() {
     //alert(rollDice());
-    diceChecker(rollDice(),'player4');
+    //diceChecker(rollDice());
+    render();
 }
 
---
+++ readme.md
@@ -0,0 +1,12 @@
+# SNES CSS Ludo
+
+Lugo game made with combination of CSS and Bootstrap, inspired by SNES design.
+
+## Practice:
+
+- positioning: absolute, relative
+- CSS grid and flexbox
+- Bootstrap responsive design
+- CSS animation
+
+[Github Pages](https://anabrd.github.io/snes-css-ludo/)
