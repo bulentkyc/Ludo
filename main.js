77abd3bf090dbacbc7cd80165102c92e9aa6ae5b
BULENT KAYICI <bulentkyc@MacBook-Pro.fritz.box>
2021-01-11 16:21:52 +0100

    v2

--- main.js
+++ main.js
@@ -34,6 +34,10 @@ const initialState = document.getElementById('main-board').innerHTML;
 let currentPlayer = 0;
 let currentPawn;
 let lastDice;
+let roundCount = 0;
+let activeRollCount = 0;
+let isLocked = false;
+
 
 function rollDice() {
     const dice =  Math.floor(Math.random() * 6) + 1;
@@ -46,23 +50,48 @@ function movePawn(player, pawnIndex, dice) {
 }
 
 function diceChecker(dice) {
-    if (dice < 6 && players[currentPlayer]['allAtHome']) {
+
+    lastDice = dice;
+
+
+    if (dice < 6 && players[currentPlayer]['activePawnCount'] == 0 ) {
         alert('Hey '+ players[currentPlayer]['userName'] +' Please wait for 6! You have ' + dice);
-    } else if (dice == 6 && players[currentPlayer]['allAtHome']) { //First 6
+    } else if (dice == 6 && players[currentPlayer]['activePawnCount']  == 0) { //First 6
         players[currentPlayer]['positions'] = [1,0,0,0];
-        players[currentPlayer].allAtHome = false;
+        players[currentPlayer].activePawnCount += 1;
+        activeRollCount--;
         alert('Congrats you can go out!');
-    } else if (dice < 6  && !players[currentPlayer]['allAtHome']) {
-        movePawn(currentPlayer, 0, dice);
-    } else if (dice == 6  && !players[currentPlayer]['allAtHome']) {
-        alert('You have 6 [lease chose a pawn!');
-        lastDice = dice;
+    } 
+    else if (dice < 6  && players[currentPlayer]['activePawnCount'] > 1) {
+        isLocked = true;
+
         if (currentPawn == undefined) {
             
         } else {
             if (players[currentPlayer].positions[currentPawn] == 0) {
                 //TO-DO: Hey this works just for red color. Do for others too!
                 players[currentPlayer].positions[currentPawn] = 1;
+                players[currentPlayer].activePawnCount += 1;
+            } else {
+                players[currentPlayer].positions[currentPawn] += lastDice;
+            }
+        }
+
+
+    }else if (dice < 6  && players[currentPlayer]['activePawnCount'] == 1) {
+        movePawn(currentPlayer, 0, dice); 
+    } else if (dice == 6  && players[currentPlayer]['activePawnCount'] > 0) {
+        alert('You have 6 please chose a pawn!');
+
+        isLocked = true;
+        
+        if (currentPawn == undefined) {
+            
+        } else {
+            if (players[currentPlayer].positions[currentPawn] == 0) {
+                //TO-DO: Hey this works just for red color. Do for others too!
+                players[currentPlayer].positions[currentPawn] = 1;
+                players[currentPlayer].activePawnCount += 1;
             } else {
                 players[currentPlayer].positions[currentPawn] += 6;
             }
@@ -72,10 +101,13 @@ function diceChecker(dice) {
 
 function currentPawnDet(id) {
     currentPawn = parseInt(id.slice(-1));
-    console.log(currentPawn);
-    diceChecker(lastDice);
-    render();
+    //console.log(currentPawn);
+    if (isLocked) {
+        diceChecker(lastDice);
+        render();
+    }
     currentPawn = undefined;
+    isLocked = false;
 }
 
 
@@ -119,8 +151,51 @@ function render () {
 
 render();
 
+function nextPlayer() {
+    currentPlayer = (currentPlayer + 1) % 4;
+}
+
 function iterateGame() {
-    //alert(rollDice());
-    diceChecker(rollDice());
-    render();
+
+    //Main case, like after first round
+    if (roundCount > 0) {
+    
+        //PLayer has more then 1 pawn in the game and should complete the round
+        if (isLocked) {
+            alert('Hey you can not roll the dice. You should wait ' + players[currentPlayer].userName)
+        } else {
+            //No pawn in game and player should roll 3 times
+            if (players[currentPlayer].activePawnCount == 0 && activeRollCount < 3) {
+                activeRollCount++;
+                diceChecker(rollDice());
+                render();
+            } 
+            //PLayer has 6 should roll again
+            else if (lastDice == 6) {
+                diceChecker(rollDice());
+                render();
+            }
+            else {
+                nextPlayer();
+                activeRollCount = 1;
+                diceChecker(rollDice());
+                render();
+            }
+
+            
+        }
+    }
+    //First round 
+    else {
+       /* 
+        let red = rollDice();
+        let green = rollDice();
+        let yellow = rollDice();
+        let blue = rollDice();
+        */
+        let starter = Math.floor(Math.random()*4);
+        alert('First player is ' + players[starter].color);
+        currentPlayer = starter;
+    }
+    roundCount += 1;
 }
